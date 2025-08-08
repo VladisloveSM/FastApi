37feda5c87e97f2e6d5d24cb29425dcbda8afd01
VladisloveSM <mvladislav610@gmail.com>
2025-08-08 16:47:52 +0300

    Add calculation endpoint to process JSON input

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi import Request
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return FileResponse("index.html")
+
+@app.post("/calculate")
+async def calculate(request: Request):
+    data = await request.json()
+    num1 = data.get("num1")
+    num2 = data.get("num2")
+    return {"result": num1 + num2}
