fe20f76012a63160c7e6922c9517882ba040b0d0
Мазур Владислав Сергеевич <vladislav.mazur@softclub.by>
2026-01-06 13:14:53 +0300

    Refactor Feedback model to include field constraints for name and message

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class User(BaseModel):
     age: int
     name: str
 
 class Feedback(BaseModel):
-    name: str
-    message: str
+    name: str = Field(max_length=50)
+    message: str = Field(max_length=50)
