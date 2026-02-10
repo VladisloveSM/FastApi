16b2b963be5057b378325af20af0e9905414f3d8
VladisloveSM <mvladislav610@gmail.com>
2026-02-10 17:22:38 +0300

    Remove unused header endpoints and CommonHeaders model from the application

--- app/models.py
+++ app/models.py
@@ -1,43 +1,12 @@
-from fastapi import HTTPException, Header
+from fastapi import Header
 from pydantic import BaseModel, Field, field_validator, EmailStr
 from typing import Optional
 import app.validators as validators
 
 
-class CommonHeaders(BaseModel):
-    user_agent: str
-    accept_language: str
-    x_current_version: str
-
-    @field_validator('user_agent')
-    def verify_user_agent(cls, value) -> str:
-        return validators.verify_user_agent(value)
-
-    @field_validator('accept_language')
-    def verify_accept_language(cls, value) -> str:
-        return validators.varify_accept_language(value)
-    
-    @field_validator('x_current_version')
-    def verify_x_current_version(cls, value) -> str:
-        return validators.verify_x_current_version(value)
-    
-
-    def get_common_headers(
-        user_agent: Optional[str] = Header(None, alias="User-Agent"),
-        accept_language: Optional[str] = Header(None, alias="Accept-Language"),
-        x_current_version: Optional[str] = Header(None, alias="X-Current-Version")
-    ):
-        return CommonHeaders(
-            user_agent=user_agent,
-            accept_language=accept_language,
-            x_current_version=x_current_version
-        )
-
-
-class LoginData(BaseModel):
+class User(BaseModel):
     username: str
     password: str
-
 
 class Contact(BaseModel):
     email: EmailStr
