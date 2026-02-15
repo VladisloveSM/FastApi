70ff1d890d522f4a24d6ca88eb7ef511e696453e
VladisloveSM <mvladislav610@gmail.com>
2026-02-15 15:35:09 +0300

    Refactor user authentication logic and add user registration endpoint for improved functionality and clarity

--- app/main.py
+++ app/main.py
@@ -9,20 +9,28 @@ security = HTTPBasic()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 fake_users_db = {
-    UserInDB(username="admin", hashed_password=pwd_context.hash("secretpassword")),
-    UserInDB(username="user", hashed_password=pwd_context.hash("userpass123"))
+    "admin": UserInDB(
+        username="admin", 
+        hashed_password=pwd_context.hash("secretpassw")
+    ),
+    "user": UserInDB(
+        username="user", 
+        hashed_password=pwd_context.hash("userpass123")
+    )
 }
 
 feedbacks = []
 
 def auth_user(credentials: HTTPBasicCredentials = Depends(security)):
 
-    user = None
-
-    for user_in_db in fake_users_db:
-        if secrets.compare_digest(user_in_db.username, credentials.username):
-            user = user_in_db
-            break
+    user = fake_users_db.get(credentials.username)
+    
+    if user is None or not secrets.compare_digest(user.username, credentials.username):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверное имя пользователя или пароль",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
     if user is None:
         raise HTTPException(
@@ -45,6 +53,14 @@ def auth_user(credentials: HTTPBasicCredentials = Depends(security)):
     
     return user
 
+@app.post("/register")
+async def register(user: User):
+    fake_users_db.append(UserInDB(username=user.username, hashed_password=pwd_context.hash(user.password)))
+    return {"message": f"User {user.username} registered successfully!"}
+
+@app.get("/users")
+async def read_users():
+    return fake_users_db
 
 @app.post("/feedback")
 async def create_feedback(feedback: Feedback, is_premium: bool = False):
