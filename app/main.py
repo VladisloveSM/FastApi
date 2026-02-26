02494be73588f5e79819a7cf0838ce26c3fef0ea
VladisloveSM <mvladislav610@gmail.com>
2026-02-26 09:31:17 +0300

    Refactor main.py by removing unused imports and add security.py for JWT token handling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.openapi.utils import get_openapi
-from app.models import Feedback, User, UserInDB
+from fastapi import FastAPI
+from app.models import Feedback
 from app.config import load_config
 from passlib.context import CryptContext
 import secrets
