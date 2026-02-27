bc78875d586052733543f638889d16e642ff8389
VladisloveSM <mvladislav610@gmail.com>
2026-02-27 11:51:37 +0300

    Implement user login functionality and secure user information retrieval

--- app/db.py
+++ app/db.py
@@ -6,10 +6,6 @@ USERS_DATA = [
 ]
 
 def get_user(username: str):
-    """
-    Функция для поиска пользователя по имени пользователя. 
-    В реальном проекте это должно быть запросом к базе данных.
-    """
     for user in USERS_DATA:
         if user.get("username") == username:
             return user
--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
-from app.models import Feedback
+from fastapi import FastAPI, Depends
+from app.models import Feedback, User
 from app.config import load_config
 from passlib.context import CryptContext
-import secrets
+from app.db import get_user, USERS_DATA
+from security import create_jwt_token, get_user_from_token
 
 app = FastAPI()
 
@@ -10,9 +11,28 @@ config = load_config()
 
 feedbacks = []
 
-@app.get("/config")
-async def read_config():
-    return {"mode": f"{config.mode}"}
+@app.post("/login")
+async def login(user_in: User): 
+    for user in USERS_DATA:
+        if user.get("username") == user_in.username and user.get("password") == user_in.password:
+            # If the verification is successful, generate a token for the user
+            token = create_jwt_token({"sub": user_in.username})  # 'sub' stands for subject, in our case the user's name
+            return {"access_token": token, "token_type": "bearer"}
+    return {"error": "Invalid credentials"}
+
+
+# Secure route that returns user information
+@app.get("/about_me")
+async def about_me(current_user: str = Depends(get_user_from_token)):
+    """
+    Этот маршрут защищен и требует токен. Если токен действителен, мы возвращаем информацию о пользователе.
+    """
+    user = get_user(current_user)
+    if user:
+        return user
+    
+    return {"error": "User not found"}
+
 
 @app.post("/feedback")
 async def create_feedback(feedback: Feedback, is_premium: bool = False):
