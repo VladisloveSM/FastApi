2c069282432ad83d0a5122e3e8ab03acad27aa10
VladisloveSM <mvladislav610@gmail.com>
2026-03-17 11:48:47 +0300

    CODE with working token-authorization system

--- app/db.py
+++ app/db.py
@@ -1,5 +1,3 @@
-import secrets
-from fastapi import HTTPException
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -20,12 +18,3 @@ USERS_DATA = [
         "password": pwd_context.hash("pass")
     },
 ]
-
-def get_user(username: str, password: str):
-    for user in USERS_DATA:
-        if secrets.compare_digest(user.get("username").encode("utf-8"), username.encode("utf-8")):
-            if pwd_context.verify(password, user.get("password")):
-                return True
-            else:
-                raise HTTPException(status_code=401, detail=f"Authorization failed")
-    raise HTTPException(status_code=404, detail="User not found")
